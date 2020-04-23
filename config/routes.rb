51fe97ba421219b211556739cd3032231ad7c28d
kotaromuto <kotaromuto19931004@gmail.com>
2020-04-24 00:26:58 +0900

    [Update]フォローボタン

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,5 @@ Rails.application.routes.draw do
   devise_for :users
   resources :users,only: [:show,:index,:edit,:update]
   resources :books
-
+  resources :relationships, only: [:create, :destroy]
 end
